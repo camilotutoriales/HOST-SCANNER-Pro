7bbdec8a97e1cb2eb53863087095ab9198dd4a8b
camilotutoriales <46059590+camilotutoriales@users.noreply.github.com>
2019-02-15 18:06:07 -0500

    archivo donde se instalaran complementos

---
+++ Dependencies.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+clear
+sleep 2
+pkg install curl
+pkg install libtool
+pkg install libcurl
+pkg install wget
+pkg install util-linux
+pkg install ruby
+gem install lolcat
+pkg install figlet
+pkg install nmap
+pkg install nodejs-lts
+npm install -g subquest-cli
+pip install speedtest-cli
+figlet LA | lolcat
+figlet INSTALACION | lolcat
+figlet AH | lolcat
+figlet TERMINADO!! | lolcat
